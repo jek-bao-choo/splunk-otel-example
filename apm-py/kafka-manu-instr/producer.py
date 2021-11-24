b99b26e93bf539c6d335783574306cb8ab5fe613
Jek <jchoo@splunk.com>
2021-11-24 17:01:25 +0800

    add traceparent to headers

--- apm-py/kafka-manu-instr/producer.py
+++ apm-py/kafka-manu-instr/producer.py
@@ -9,11 +9,13 @@ from kafka import KafkaProducer # For Kafka
 
 # Start of OTel part 2
 tracer = trace.get_tracer_provider().get_tracer(__name__)
-headers = {}
 
 with tracer.start_as_current_span("jek_kafka_producer_span"):
+    headers = {}
     inject(headers)
     print("***********headers", headers)
+    byte_traceparent = headers['traceparent'].encode('utf-8')
+    print("********byte traceparent", byte_traceparent)
 # Start of OTel part 2
 
 
@@ -21,11 +23,10 @@ with tracer.start_as_current_span("jek_kafka_producer_span"):
 # For Kafka to use single server instead of cluster Zookeeper
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
-
 # For Kafka and will create the topic automatically
 # Convert string to bytes
 future = producer.send('jek-kafka-topic',
-                       # headers=headers,
-                       value=b'Hello Jek')
+                       headers=[('traceparent', byte_traceparent)],
+                       value=b'Hello Jek 24 Nov')
 result = future.get(timeout=60)  # Block until a single message is sent (or timeout)
 # End of Kafka part
