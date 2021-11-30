80f46302404561c1f6e571ecdcb42a059afdb5dd
Jek <jchoo@splunk.com>
2021-12-01 05:47:24 +0800

    Add comment to logging for nestjs pinojs

--- apm-js/nestjs-pinojs-auto-instr/src/app.module.ts
+++ apm-js/nestjs-pinojs-auto-instr/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggerModule } from './logger/logger.module';
+import { LoggerModule } from './logger/logger.module'; // added to do logging with pino
 
 @Module({
-  imports: [LoggerModule],
+  imports: [LoggerModule], // added to do logging with pino
   controllers: [AppController],
   providers: [AppService],
 })
