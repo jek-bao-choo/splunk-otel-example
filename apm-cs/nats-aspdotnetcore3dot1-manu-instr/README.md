59bd33d987f6db8eed52b64069549e67d7530d25
Jek <jchoo@splunk.com>
2021-12-14 14:57:05 +0800

    initial commit of nats without tracing yet

---
+++ apm-cs/nats-aspdotnetcore3dot1-manu-instr/README.md
@@ -0,0 +1,16 @@
+﻿This is WIP.
+Next step containerised with Docker so that can add CLR profiler and run it with Profiler.
+After adding profiler is to print with trace id and span id to ensure it has local context.
+
+Terminal 1 in subscriber folder
+
+$ `cd subscriber`
+
+$ `dotnet run -verbose`
+
+
+Terminal 2 in publisher folder run
+
+$ `cd publisher`
+
+$ `dotnet run -payload helloworldv1`
---
+++ apm-cs/nats-aspdotnetcore3dot1-manu-instr/publisher/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Diagnostics;
+using System.Text;
+using NATS.Client;
+
+namespace publisher
+{
+    // Based mostly on:
+    // https://github.com/nats-io/nats.net/tree/master/src/Samples/Publish
+    public class Program
+    {
+
+        Dictionary<string, string> parsedArgs = new Dictionary<string, string>();
+
+        int count = 1;
+        string url = Defaults.Url;
+        string subject = "jekSubject";
+        byte[] payload = null;
+        string creds = null;
+
+        public void Run(string[] args)
+        {
+            Stopwatch sw = null;
+
+            parseArgs(args);
+            banner();
+
+            Options opts = ConnectionFactory.GetDefaultOptions();
+            opts.Url = url;
+            if (creds != null)
+            {
+                opts.SetUserCredentials(creds);
+            }
+
+            using (IConnection c = new ConnectionFactory().CreateConnection(opts))
+            {
+                sw = Stopwatch.StartNew();
+
+                for (int i = 0; i < count; i++)
+                {
+                    // todo Jek: Add in inject context propagation try to refer kafka example https://github.com/signalfx/signalfx-dotnet-tracing/tree/main/samples
+                    c.Publish(subject, payload); 
+                }
+                c.Flush();
+
+                sw.Stop();
+
+                Console.Write("Published {0} msgs in {1} seconds ", count, sw.Elapsed.TotalSeconds);
+                Console.WriteLine("({0} msgs/second).",
+                    (int)(count / sw.Elapsed.TotalSeconds));
+                printStats(c);
+
+            }
+        }
+
+        private void printStats(IConnection c)
+        {
+            IStatistics s = c.Stats;
+            Console.WriteLine("Statistics:  ");
+            Console.WriteLine("   Outgoing Payload Bytes: {0}", s.OutBytes);
+            Console.WriteLine("   Outgoing Messages: {0}", s.OutMsgs);
+        }
+
+        private void usage()
+        {
+            Console.Error.WriteLine(
+                "Usage:  Publish [-url url] [-subject subject] " +
+                "[-count count] [-creds file] [-payload payload]");
+
+            Environment.Exit(-1);
+        }
+
+        private void parseArgs(string[] args)
+        {
+            if (args == null)
+                return;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i + 1 == args.Length)
+                    usage();
+
+                parsedArgs.Add(args[i], args[i + 1]);
+                i++;
+            }
+
+            if (parsedArgs.ContainsKey("-count"))
+                count = Convert.ToInt32(parsedArgs["-count"]);
+
+            if (parsedArgs.ContainsKey("-url"))
+                url = parsedArgs["-url"];
+
+            if (parsedArgs.ContainsKey("-subject"))
+                subject = parsedArgs["-subject"];
+
+            if (parsedArgs.ContainsKey("-payload"))
+                payload = Encoding.UTF8.GetBytes(parsedArgs["-payload"]);
+
+            if (parsedArgs.ContainsKey("-creds"))
+                creds = parsedArgs["-creds"];
+        }
+
+        private void banner()
+        {
+            Console.WriteLine("Publishing {0} messages on subject {1}",
+                count, subject);
+            Console.WriteLine("  Url: {0}", url);
+            Console.WriteLine("  Subject: {0}", subject);
+            Console.WriteLine("  Count: {0}", count);
+            Console.WriteLine("  Payload is {0} bytes.",
+                payload != null ? payload.Length : 0);
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                new Program().Run(args);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Exception: " + ex.Message);
+                Console.Error.WriteLine(ex);
+            }
+
+        }
+
+
+    }
+}
