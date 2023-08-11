88ba665dc57d9c42cd79bdeaa6f27bb7d9221106
Jek <jchoo@splunk.com>
2023-08-11 19:11:21 +0800

    add otel-erlang
    Pending Tristan to verify why no traces

--- apm-erl/helloworld/mywebapp/src/hello_handler.erl
+++ apm-erl/helloworld/mywebapp/src/hello_handler.erl
@@ -2,9 +2,23 @@
 -behaviour(cowboy_handler).
 -export([init/2]).
 
-init(Req0, State) ->
-  Req = cowboy_req:reply(200,
-    #{<<"content-type">> => <<"text/plain">>},
-    <<"Hello Jek!">>,
-    Req0),
-  {ok, Req, State}.
+-include_lib("opentelemetry_api/include/otel_tracer.hrl").
+
+init(Req, State) ->
+    hello(),
+    {ok, cowboy_req:reply(200, #{}, <<"Hello, Jek!">>, Req), State}.
+
+hello() ->
+    %% start an active span and run a local function
+    ?with_span(<<"operation">>, #{}, fun nice_operation/1).
+
+nice_operation(_SpanCtx) ->
+    ?add_event(<<"Nice operation!">>, [{<<"bogons">>, 100}]),
+    ?set_attributes([{another_key, <<"yes">>}]),
+
+    %% start an active span and run an anonymous function
+    ?with_span(<<"Sub operation...">>, #{},
+                fun(_ChildSpanCtx) ->
+                      ?set_attributes([{lemons_key, <<"five">>}]),
+                      ?add_event(<<"Sub span event!">>, [])
+                end).
