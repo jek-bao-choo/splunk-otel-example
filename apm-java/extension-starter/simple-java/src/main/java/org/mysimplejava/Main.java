47ead6879b35d35132c3ebe339f5d391e8789b0c
Jek <jchoo@splunk.com>
2023-08-28 14:45:50 +0800

    add my-simple-java module

--- apm-java/extension-starter/simple-java/src/main/java/org/mysimplejava/Main.java
+++ apm-java/extension-starter/simple-java/src/main/java/org/mysimplejava/Main.java
@@ -1,19 +1,37 @@
 package org.mysimplejava;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
             System.out.println("i = " + i);
         }
+
+        // write a program waiting for user input and count the number of words in the sentence
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String i = scanner.nextLine();
+        Main m = new Main();
+        int number = m.countWords(i);
+        System.out.println("Your sentence has: " + number + " words.");
+    }
+
+    public int countWords(String words) {
+        try {
+            Thread.sleep(19876);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (words == null || words.isEmpty()) {
+            return 0;
+        }
+
+        String[] afterSplit = words.split("\\s+");
+        return afterSplit.length;
     }
 }
