0c1ed132093c89012ff14eee9952d9e0a1f40b97
Jek <jchoo@splunk.com>
2024-11-01 16:22:57 +0800

    working locally but not working after deployed.

--- apm-js/lambda-sam-nodejs18-hapijs-typescript/hello-world/app.ts
+++ apm-js/lambda-sam-nodejs18-hapijs-typescript/hello-world/app.ts
@@ -1,4 +1,20 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { Server } from '@hapi/hapi';
+import { StaticServePlugin } from './StaticServePlugin';
+
+let server: Server;
+
+async function initServer(): Promise<Server> {
+    const server = new Server({
+        port: 3000,
+        host: 'localhost',
+    });
+
+    // Register the static serve plugin
+    await server.register([new StaticServePlugin()]);
+
+    return server;
+}
 
 /**
  *
@@ -12,22 +28,32 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
+        server = await initServer();
+
+        // Make sure to include the path from the event
+        const response = await server.inject({
+            method: event.httpMethod,
+            url: event.path || '/', // Ensure URL is always defined
+            // payload: event.body,
+            headers: event.headers,
+        });
+
         return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world with lambda layer WITHOUT HAPIJS',
-            }),
+            statusCode: response.statusCode,
+            body: response.payload,
+            headers: {
+                'Content-Type': 'application/json',
+            },
         };
     } catch (err) {
         console.log(err);
         return {
             statusCode: 500,
             body: JSON.stringify({
-                message: 'some error happened',
+                message: 'JEK ERROR some error happened',
             }),
         };
     }
 };
-
 
 module.exports = { lambdaHandler };
