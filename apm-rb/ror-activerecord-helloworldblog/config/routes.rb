90f1e5af3c1be976d09f02b874bd6923bec57f50
Jek <jchoo@splunk.com>
2024-11-05 14:20:05 +0800

    rails generate controller Posts index new create

--- apm-rb/ror-activerecord-helloworldblog/config/routes.rb
+++ apm-rb/ror-activerecord-helloworldblog/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "posts/index"
+  get "posts/new"
+  get "posts/create"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
---
+++ apm-rb/ror-activerecord-helloworldblog/test/controllers/posts_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class PostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get posts_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get posts_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get posts_create_url
+    assert_response :success
+  end
+end
