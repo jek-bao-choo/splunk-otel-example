5d92ef15289caf1adbbdb92dae067e7fdb3b1e1b
Jek <jchoo@splunk.com>
2024-11-05 12:02:26 +0800

    init

---
+++ apm-rb/ror-active-record-simple-blog/app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
+
